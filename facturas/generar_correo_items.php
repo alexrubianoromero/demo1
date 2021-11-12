4319eabaa3bcadd36f57d04270dd79dd2e6b6e3d
Alex Rubiano <alexrubianoromero@gmail.com>
2021-11-12 09:57:21 -0500

    cambios para envio de facturaas pór correo 3

--- facturas/generar_correo_items.php
+++ facturas/generar_correo_items.php
@@ -1,9 +1,9 @@
 <?php
 
-echo '<pre>';
-print_r($_REQUEST);
-echo '</pre>';
-die();
+// echo '<pre>';
+// print_r($_REQUEST);
+// echo '</pre>';
+// die();
 include('../valotablapc.php');
 
 $sqlFacturaTraeIdOrden = "select * from $tabla11 where id_factura = '".$_REQUEST['id_factura']."' "; 
@@ -18,31 +18,18 @@ $placa = $arregloIdOrden['placa'];
 $sql_correo = "select cli.email as email from $tabla14 o
 inner join $tabla4 c on (c.placa = o.placa)
 inner join $tabla3 cli on (cli.idcliente=c.propietario)
-where o.id = '".$_REQUEST['idorden']."'
+where o.id = '".$idOrden."'
 ";
 $consulta_email= mysql_query($sql_correo,$conexion);
 $arreglo_email = mysql_fetch_assoc($consulta_email);
 $email = $arreglo_email['email'];
 // echo  '<br>'.$email;
 // die();
-$sql_items= "select * from $tabla15 where no_factura =   '".$_REQUEST['idorden']."'  ";
 
-//echo '<br>'.$sql_items;
-$consulta_items = mysql_query($sql_items);
-$texto_items ='';
-$suma_items = '0';
-while($items = mysql_fetch_assoc($consulta_items))
-{	
- $texto_items = $texto_items.'*'.$items['descripcion'];
- $suma_items += $items['total_item'];
- //echo '<br>'.$items['descripcion'];
-}
-//echo '<br>'.$texto_items;
-//echo '<br>'.$suma_items;
 
 $body = 'KAYMO
 
-Te informa el avance de tu orden de reparacion
+Te informa que el documento de tu reparacion lo puedes ver en el siguiente link
 
 Placa: '.$placa.'  Factura No: '.$noFactura.'
 
@@ -57,7 +44,8 @@ Taller KAYMO
 
 O envianos un E-mail a ventas@alexrubiano.com 
 Recuerda, estamos ubicados en la cll 26 con 73';
-
+// echo $body;
+// die();
 //echo '<br>'.$texto_items;
 
 //echo '<br>body<br>'.$body.'<br>fin de body</br>';
